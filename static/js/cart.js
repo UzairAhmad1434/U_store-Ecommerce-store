a49709246d2f7cf5947829d8a469fee7c552729d
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-08-05 17:53:57 +0500

    add to cart1

---
+++ static/js/cart.js
@@ -0,0 +1,45 @@
+let updateBtns = document.getElementsByClassName("update-cart");
+
+for (let i = 0; i < updateBtns.length; i++) {
+  updateBtns[i].addEventListener("click", function () {
+    let productId = this.dataset.product;
+    let action = this.dataset.action;
+
+    console.log(productId, action);
+
+    if (user === "AnonymousUser") {
+      console.log("User is not authenticated");
+    } else {
+      updateUserOrder(productId, action);
+    }
+  });
+}
+
+function updateUserOrder(productId, action) {
+  console.log("User is authenticated, sending data...");
+  let url = "/updateItem/";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data:", data);
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    });
+}
--- store/views.py
+++ store/views.py
@@ -1,31 +1,63 @@
 from django.shortcuts import render
-from .models import * 
+from .models import *
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def store(request):
-	products = Product.objects.all()
-	context = {'products':products}
-	return render(request, 'store/store.html', context)
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-	else:
-		items = []
-		order={'get_cart_total':0,'get_cart_items':0}
-
-	context = {'items':items,'order':order}
-	return render(request, 'store/cart.html', context)
+    context = {'items': items, 'order': order}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-	else:
-		items = []
-		order={'get_cart_total':0,'get_cart_items':0}
-	context = {'items':items,'order':order}
-	return render(request, 'store/checkout.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html', context)
+
+@csrf_exempt
+def updateItem(request):
+    if request.method == 'POST':
+        # try:
+            data = json.loads(request.body)
+            productId = data.get('productId')
+            action = data.get('action')
+            
+            # if not productId or not action:
+            #     return JsonResponse({'error': 'Missing productId or action'}, status=400)
+
+            customer = request.user.customer
+            product = Product.objects.get(id=productId)
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+            if action == 'add':
+                orderItem.quantity += 1
+            elif action == 'remove':
+                orderItem.quantity -= 1
+
+            orderItem.save()
+
+            if orderItem.quantity <= 0:
+                orderItem.delete()
+
+            return JsonResponse('Item was added', safe=False)
+
